8cfa8aefa959474b24f6b922450e236e6b9a4c8b
Erish <irishmjn@gmail.com>
2025-09-16 17:47:53 +0545

    extended new file from employee permissions

--- projects/permissions.py
+++ projects/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 from employee.models import Employee
 
 class IsAssignedEmployeeOrReviewer(permissions.BasePermission):
@@ -29,5 +30,30 @@ class IsAssignedEmployeeOrReviewer(permissions.BasePermission):
             Employee.ADMIN
         ]:
             return True
+
+        return False
+    
+
+
+class IsAssignedProjectOrHigher(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        user_role = request.user.employeeprofile.role
+        return user_role in ['HR', 'TEAM_LEAD', 'PROJECT_MANAGER', 'ADMIN']
+
+    def has_object_permission(self, request, view, obj):
+        user_profile = request.user.employeeprofile
+        user_role = user_profile.role
+
+        # Higher roles always allowed
+        if user_role in ['HR', 'TEAM_LEAD', 'PROJECT_MANAGER', 'ADMIN']:
+            return True
+
+        # Normal employee → only view assigned projects
+        if request.method in SAFE_METHODS:
+            return user_profile in obj.members.all() \
+                    or user_profile == obj.manager \
+                    or user_profile == obj.team_lead
 
         return False
