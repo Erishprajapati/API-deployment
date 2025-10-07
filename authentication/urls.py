8e480b5eb5ba69499ba02271c7112fefdd4bb7ed
Erish <irishmjn@gmail.com>
2025-10-07 10:13:39 +0545

    Commit for redeployment services

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,13 +1,13 @@
 # authentication/urls.py
 from django.urls import path, include
 from .views import *
-from .views_google import GoogleAuthViewSet
+# from .views_google import GoogleAuthViewSet
 from rest_framework.routers import DefaultRouter
 
 app_name = "authentication"
 
-router = DefaultRouter()
-router.register(r'google', GoogleAuthViewSet, basename='google-auth')
+# router = DefaultRouter()
+# router.register(r'google', GoogleAuthViewSet, basename='google-auth')
 
 urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup_view"),
@@ -16,5 +16,5 @@ urlpatterns = [
     path("refresh-token/", RefreshTokenView.as_view(), name="refresh_token_view"),
     # path("protected-view/", ProtectedView.as_view(), name="protected_view"),
 
-    path("", include(router.urls)),
+    # path("", include(router.urls)),
 ]
