1436b628dd5b21ec9c07fe5db3e85ac520b79ac8
Erish <irishmjn@gmail.com>
2025-10-14 17:35:09 +0545

    Recent pushes to project

--- project_management/settings.py
+++ project_management/settings.py
@@ -226,3 +226,11 @@ SOCIALACCOUNT_PROVIDERS = {
         'METHOD': 'oauth2',
     }
 }
+"""connects Django → Redis → Celery."""
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+#for the fake email to test celery and redis
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
