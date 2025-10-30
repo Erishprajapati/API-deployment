af891a697371117f97c9d3841af991bdf41e43d4
Erish <irishmjn@gmail.com>
2025-10-30 11:26:20 +0545

    ..

--- project_management/settings.py
+++ project_management/settings.py
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
-
+# AUTH_USER_MODEL = "employee.User"
 
 # Internationalization
 LANGUAGE_CODE = 'en-us'
