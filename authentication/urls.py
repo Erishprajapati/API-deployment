a6df5b24bba7406569ec885aad263a550450afbc
Erish <irishmjn@gmail.com>
2025-11-09 17:36:52 +0545

    Add: Configurations of urls for google login

--- authentication/urls.py
+++ authentication/urls.py
@@ -14,7 +14,6 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login_view"),
     path("logout/", LogoutView.as_view(), name="logout_view"),
     path("refresh-token/", RefreshTokenView.as_view(), name="refresh_token_view"),
-    # path("protected-view/", ProtectedView.as_view(), name="protected_view"),
-
-    # path("", include(router.urls)),
+    path('api/social-login-success/', social_login_success, name='social-login-success')
 ]
+
