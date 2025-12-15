400eba48fd724e01c0302fa66621771d79e7195f
Erish <irishmjn@gmail.com>
2025-12-15 15:12:50 +0545

    redeployed the postgres database in render

--- project_management/settings.py
+++ project_management/settings.py
@@ -60,7 +60,8 @@ ROOT_URLCONF = 'project_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # ← Add this
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
